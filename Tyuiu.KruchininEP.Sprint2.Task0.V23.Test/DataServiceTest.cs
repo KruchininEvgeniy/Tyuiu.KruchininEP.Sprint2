a84f3c5429dc00a454ceb394edca4f7a8c95f346
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-10-06 19:26:08 +0500

    Add project files.

---
+++ Tyuiu.KruchininEP.Sprint2.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Tyuiu.KruchininEP.Sprint2.Task0.V23.Lib;
+
+namespace Tyuiu.KruchininEP.Sprint2.Task0.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int x = 105;
+            int y = 795;
+
+            bool[] result = new bool[6];
+            result = ds.GetCompareOperations(x, y);
+            string string_result = string.Join(", ", result);
+            Assert.AreEqual("True, False, True, True, False, True", string_result);
+        }
+    }
+}
