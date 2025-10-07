2b151059dd43f5c8b16942080515c8101af7b55a
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-10-07 21:48:16 +0500

    Task2

---
+++ Tyuiu.KruchininEP.Sprint2.Task2.V11.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.KruchininEP.Sprint2.Task2.V11.Lib;
+
+namespace Tyuiu.KruchininEP.Sprint2.Task2.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidChekDotInShadedArea()
+        {
+            DataService ds = new DataService();
+
+            int x = 12;
+            int y = 11;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(true, res);
+        }
+    }
+}
