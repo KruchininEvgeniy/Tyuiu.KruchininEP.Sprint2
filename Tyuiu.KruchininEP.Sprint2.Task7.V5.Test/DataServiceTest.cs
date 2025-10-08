83800566632392f57829e692d554b851540ae2f6
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-10-08 11:08:40 +0500

    Task7

---
+++ Tyuiu.KruchininEP.Sprint2.Task7.V5.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿
+using Tyuiu.KruchininEP.Sprint2.Task7.V5.Lib;
+
+namespace Tyuiu.KruchininEP.Sprint2.Task7.V5.Test
+{
+
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+
+            double x = 0;
+            double y = 0.5;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+
+
+        }
+    }
+}
--- Tyuiu.KruchininEP.Sprint2.Task7.V5/Program.cs
+++ Tyuiu.KruchininEP.Sprint2.Task7.V5/Program.cs
@@ -1,43 +1,53 @@
-﻿using Tyuiu.KruchininEP.Sprint2.Task7.V3.Lib;
-namespace Tyuiu.KruchininEP.Sprint2.Task7.V3
+﻿
+using Tyuiu.KruchininEP.Sprint2.Task7.V5.Lib;
+
+namespace Tyuiu.KruchininEP.Sprint2.Task7.V5
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
             Console.Title = "Спринт #2 | Выполнил: Кручинин Е. П. | РППб25-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #2                                                               *");
-            Console.WriteLine("* Добавление к решению итоговых проектов по спринту                       *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
             Console.WriteLine("* Задание #7                                                              *");
-            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
             Console.WriteLine("* Выполнил: Кручинин Е. П. | РППб25-1                                     *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
             Console.WriteLine("* Написать программу на C#, которая запрашивает исходные данные           *");
             Console.WriteLine("* (вещественные значения) и вычисляет, находится ли точка с               *");
-            Console.WriteLine("* координатами X,Y в заштрихованной области                               *");
-            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* координатами X,Y в заштрихованной области.                              *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine("Введите значение X: ");
-            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Введите значение Y: ");
-            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            bool res = ds.CheckDotInShadedArea(x, y);
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            DataService ds = new DataService();
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в закрашенной области");
+            }
 
-            var result = ds.CheckDotInShadedArea(x, y);
 
-            Console.WriteLine(result);
             Console.ReadKey();
         }
+
     }
 }
