62703c63dd35081ddb1e66de133b2e5bf9c0889d
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-10-08 10:25:33 +0500

    Task3

---
+++ Tyuiu.KruchininEP.Sprint2.Task3.V23/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using Microsoft.Win32.SafeHandles;
+using Tyuiu.KruchininEP.Sprint2.Task3.V23.Lib;
+
+namespace Tyuiu.KruchininEP.Sprint2.Task3.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Кручинин Е. П.. | РППб25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Кручинин Е. П.  | РППб25-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+            Console.WriteLine("* с использованием вложенных оператор if-else, где пользователь           *");
+            Console.WriteLine("* вводит значение переменной X с клавиатуры.                              *");
+            Console.WriteLine("* Округлить полученное значение до трех знаков после запятой              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной Х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение Функции = " + res);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
