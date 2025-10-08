8e8fd57ec7eaf79ec8878916824f5d7d044b4c4e
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-10-08 11:03:10 +0500

    Task7

---
+++ Tyuiu.KruchininEP.Sprint2.Task7.V3/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.KruchininEP.Sprint2.Task7.V3.Lib;
+namespace Tyuiu.KruchininEP.Sprint2.Task7.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Кручинин Е. П. | РППб25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Добавление к решению итоговых проектов по спринту                       *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Кручинин Е. П. | РППб25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на C#, которая запрашивает исходные данные           *");
+            Console.WriteLine("* (вещественные значения) и вычисляет, находится ли точка с               *");
+            Console.WriteLine("* координатами X,Y в заштрихованной области                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
