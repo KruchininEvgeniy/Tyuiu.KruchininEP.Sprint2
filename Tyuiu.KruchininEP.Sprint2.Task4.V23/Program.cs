e664e099f966ca439c58bd5bec2b032f10a9349b
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-10-08 10:37:27 +0500

    Task4

---
+++ Tyuiu.KruchininEP.Sprint2.Task4.V23/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.KruchininEP.Sprint2.Task4.V23.Lib;
+namespace Tyuiu.KruchininEP.Sprint2.Task4.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Кручинин Е. П.. | РППб25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Кручинин Е. П.  | РППб25-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение                *");
+            Console.WriteLine("* с использованием тернарного оператора, где пользователь                 *");
+            Console.WriteLine("* вводит значение переменных x,y, с клавиатуры                            *");
+            Console.WriteLine("* Округлить полученное значение до трех знаков после запятой              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите значение переменной X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x, y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Значение функции = " + res);
+        }
+    }
+}
