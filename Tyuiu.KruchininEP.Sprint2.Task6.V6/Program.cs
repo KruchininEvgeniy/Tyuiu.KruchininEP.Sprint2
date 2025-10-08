0234f75c44a7ba4c0300528bd848db21c05d820f
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-10-08 10:54:49 +0500

    Task6

---
+++ Tyuiu.KruchininEP.Sprint2.Task6.V6/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.KruchininEP.Sprint2.Task6.V6.Lib;
+namespace Tyuiu.MitinNV.Sprint2.Task6.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Кручинин Е. П. | РППб25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Получение результата из switch                                          *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Кручинин Е. П. | РППб25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*Написать программу, которая использует сокращенную форму записи оператора*");
+            Console.WriteLine("*а switch вычисляет требуемое значение и возвращает результат.            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            var result = ds.FindCardNameAndValue(a, b);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
